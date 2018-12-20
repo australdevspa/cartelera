d3dbc2d449621eb07bc6abf893e841ee2411c895
laloinsane <calisto.michel@gmail.com>
2018-12-20 12:54:22 -0300

    update estilo que esta pasando hoy, visita guiada

--- src/components/CardHoy.vue
+++ src/components/CardHoy.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="uk-card uk-card-secondary uk-card-hover uk-card-body padd">
+  <!--<div class="uk-card uk-card-secondary uk-card-hover uk-card-body padd">
     <h5>{{carta.horario}} - {{carta.nombre}}</h5>
+  </div>-->
+  <!--<div class="padd">
+    <div class="uk-grid-divider uk-text-center" uk-grid>
+
+      <div class="uk-width-1-3@s">
+          <div class="uk-card uk-card-default uk-card-body">1-3</div>
+      </div>
+          <div class="uk-width-expand@s">
+        <div class="uk-card uk-card-default uk-card-body">Expand</div>
+    </div>
+
+    </div>
+
+    <h5>{{carta.horario}} | {{carta.nombre}}</h5>
+  </div>-->
+  <div class="carta-hoy">
+    <div class="uk-grid-divider uk-child-width-expand@s" uk-grid>
+        <div class="uk-width-1-3@s">
+          Hoy <br> {{carta.horario}}
+        </div>
+        <div class="uk-width-expand@s">
+          {{carta.nombre}}
+        </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +41,18 @@ export default {
 </script>
 
 <style scoped>
-.padd{
+/*.padd{
   margin: 5px;
+  background-color: white;
+  padding: 5px;
+}
+.padd h5 {
+  margin-bottom: 0px !important;
+}*/
+.carta-hoy {
+  background-color: #666;
+  padding: 10px;
+  color: white;
+  text-align: center;
 }
 </style>
