23e96fe2fded666355f82825a835ac672811532e
laloinsane <calisto.michel@gmail.com>
2018-12-26 17:50:20 -0300

    up}

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,7 @@ export default new Router({
       component: EventoView
     },
     {
-      path: '/espaciosx',
+      path: '/espaciosxx',
       name: 'VisitaGuiada',
       component: VisitaGuiadaView,
       children: [
