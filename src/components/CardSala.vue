e4d8411dacff3a342c2ee7bfe7ecc58ba34980f8
laloinsane <calisto.michel@gmail.com>
2019-01-06 16:11:33 -0300

    update espacios card-salas

--- src/components/CardSala.vue
+++ src/components/CardSala.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="uk-card uk-card-default uk-card-body">
+
+  <!--<div class="uk-card-headeruk-background-top-left uk-background-cover" :style="{ 'background': '    linear-gradient(to top, rgba(0, 0, 0, 1.0), rgba(0, 0, 0, 0.5)), url(' + sala.rutaimg + ')' }">-->
+ 
+  
+  <!--<div class="uk-card uk-card-default uk-card-body uk-background-blend-screen uk-background-primary uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + sala.rutaimg + ')' }">-->
+   <!--<div class="uk-card uk-card-default uk-card-body uk-background-top-left uk-background-cover" :style="{ 'background': '    linear-gradient( rgba(255, 0, 0, 0.45), rgba(255, 25, 184, 104) ), url(' + sala.rutaimg + ')' }">-->
+
+    
+<div class="uk-card uk-card-default">
+
+      <div class="uk-card-header uk-background-cover uk-background-top-center border" :style="{ 'background': 'linear-gradient(to top, rgba(0, 0, 0, 1.0), rgba(200, 200, 200, 0.3)), url(' + sala.rutaimg + ')' }">
+        
+        <div class="uk-card-badge-left uk-label" :style="{ background: '#19b868 !important', fontWeight: 900 }">
+          Piso
+        </div>
+        
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-expand margen">
+                <h3 class="uk-card-title uk-margin-remove-bottom sala-estilo sala-titulo">{{sala.sala}}</h3>
+                <p class="uk-margin-remove-top sala-estilo">{{sala.centro}}</p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-card-body">
+    
+      <div  v-for="(it, index) in filtro_por_actividades"
+        :key="index"
+        @click.prevent="goToActividad(it)">
+        <div v-if="sala.id === it.sala_id">
+          <CardHoy :carta="it"></CardHoy>
+        </div>
+      </div>
+
+      <div  v-for="(it, index) in filtro_por_exposiciones"
+        :key="index"
+        @click.prevent="goToExpo(it)">
+        <div v-if="sala.sala === it.sala">
+          <CardHoy :carta="it"></CardHoy>
+        </div>
+      </div>
+
+    </div>
+    <!--<div class="uk-card-footer">
+        <a href="#" class="uk-button uk-button-text">Read more</a>
+    </div>-->
+
+  </div>
+
+
+    <!--<div class="uk-card uk-card-default uk-card-body">
     <h2 class="uk-margin-remove-bottom">{{sala.sala}}</h2>
     <p class="uk-margin-remove-top">{{sala.centro}}</p>
 
@@ -18,8 +67,9 @@
         <CardHoy :carta="it" class="cursor"></CardHoy>
       </div>
     </div>
+  </div>-->
 
-  </div>
+
 </template>
 
 <script>
@@ -72,10 +122,42 @@ export default {
 </script>
 
 <style scoped>
+.uk-card-badge-left {
+    position: absolute;
+    top: 30px;
+    left: 30px; 
+    z-index: 1;
+}
 .uk-margin-remove-bottom {
     margin-bottom: 0!important;
 }
 .uk-margin-remove-top {
     margin-top: 0!important;
 }
+.margen {
+  margin: 25px !important;
+}
+.sala-estilo {
+  color: white;
+  margin: 10px !important;
+}
+.sala-titulo {
+  font-size: 32px !important;
+}
+.border {
+  border-bottom-color: black !important;
+}
+.uk-card-body {
+  background: black;
+}
+/*.tinted-image {
+  background: 
+    /* top, transparent red, faked with gradient */ 
+    /*linear-gradient(
+      rgba(255, 0, 0, 0.45), 
+      rgba(255, 0, 0, 0.45)
+    ),*/
+    /* bottom, image*/ 
+   /* url(image.jpg);
+}*/
 </style>
