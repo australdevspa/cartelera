e68e5cba87473e310bd331cbc9bd3c27072190e2
laloinsane <calisto.michel@gmail.com>
2019-03-05 22:54:02 -0300

    update best practices

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,14 +3,14 @@
     <div class="uk-container uk-container-center uk-text-center">
       <div class="uk-panel">
         <p class="uk-text-small text-footer">
-          <a href="https://www.culturapuertomontt.cl" class="a-footer" target="_blank">culturapuertomontt.cl</a> / Teléfono: 652482638
+          <a href="https://www.culturapuertomontt.cl" class="a-footer" target="_blank" rel="noreferrer">culturapuertomontt.cl</a> / Teléfono: 652482638
           <br>
           <br>
-          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.facebook.com/culturaptomontt" aria-label="Facebook" uk-icon="icon: facebook;" target="_blank"></a>
-          <a class="icono-boton uk-icon uk-margin-small-right" href="https://twitter.com/culturaptomontt" aria-label="Twitter" uk-icon="icon: twitter;" target="_blank"></a>
-          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.instagram.com/culturapuertomontt/" aria-label="Instagram" uk-icon="icon: instagram;" target="_blank"></a>
-          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.flickr.com/photos/culturapuertomontt/sets/" aria-label="Flickr" uk-icon="icon: flickr;" target="_blank"></a>
-          <a class="icono-boton uk-icon" href="https://www.youtube.com/channel/UCDlH1MHCO9PKRwn4o4fNcpw" aria-label="Youtube" uk-icon="icon: youtube;" target="_blank"></a>
+          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.facebook.com/culturaptomontt" aria-label="Facebook" uk-icon="icon: facebook;" target="_blank" rel="noreferrer"></a>
+          <a class="icono-boton uk-icon uk-margin-small-right" href="https://twitter.com/culturaptomontt" aria-label="Twitter" uk-icon="icon: twitter;" target="_blank" rel="noreferrer"></a>
+          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.instagram.com/culturapuertomontt/" aria-label="Instagram" uk-icon="icon: instagram;" target="_blank" rel="noreferrer"></a>
+          <a class="icono-boton uk-icon uk-margin-small-right" href="https://www.flickr.com/photos/culturapuertomontt/sets/" aria-label="Flickr" uk-icon="icon: flickr;" target="_blank" rel="noreferrer"></a>
+          <a class="icono-boton uk-icon" href="https://www.youtube.com/channel/UCDlH1MHCO9PKRwn4o4fNcpw" aria-label="Youtube" uk-icon="icon: youtube;" target="_blank" rel="noreferrer"></a>
           <br>
           <br>
           Copyright © {{year}} Cartelera.
