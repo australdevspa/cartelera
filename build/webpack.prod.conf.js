11a289cc6bad5d7a9f7e78f5727de950fa309d92
laloinsane <calisto.michel@gmail.com>
2019-03-17 20:22:11 -0300

    update test ws

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -128,8 +128,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/',
-      staticFileGlobsIgnorePatterns: ['dist/**/*-worker\.js/'],
+      stripPrefix: 'dist/'
+      //staticFileGlobsIgnorePatterns: ['dist/**/*-worker\.js/'],
     }),
     
     // Ignore all locale files of moment.js
