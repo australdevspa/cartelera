eba86f1d4b8efa4316ebfbb9c0974e29bc687789
laloinsane <calisto.michel@gmail.com>
2019-04-09 04:46:56 -0400

    test tour obra

--- src/views/Obra.vue
+++ src/views/Obra.vue
@@ -55,7 +55,7 @@
           </div>
 
           <div v-if="evento.video_url_tour !== null" style="padding: 30px 10px;">
-            <iframe width="100%" height="640" style="width: 100%; height: 640px; border: none; max-width: 100%;" frameborder="0" allow="vr,gyroscope,accelerometer,fullscreen" scrolling="no" allowfullscreen="true" src="https://kuula.co/share/collection/7l5Fk?fs=1&vr=0&thumbs=1&chromeless=0&logo=0"></iframe>
+            <iframe class="tour" width="100%" height="640" style="width: 100%; height: 640px; border: none; max-width: 100%;" frameborder="0" allow="vr,gyroscope,accelerometer,fullscreen" scrolling="no" allowfullscreen="true" src="https://kuula.co/share/collection/7l5Fk?fs=1&vr=0&thumbs=1&chromeless=0&logo=0"></iframe>
           </div>
 
           <!--<div class="uk-card-body uk-text-center">
@@ -294,5 +294,10 @@ iframe {
   max-height: 100% !important;
   display: block !important;
   margin: 0 auto !important;
+}
+@media only screen and (max-width: 639px) {
+  .tour {
+    height: 540px !important;
+  }
 }
 </style>
