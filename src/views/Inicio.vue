e755b1bc4fcae73b9c0a40ad63dea52a156009fd
laloinsane <calisto.michel@gmail.com>
2020-02-20 15:23:33 -0300

    subscripcion de usuarios

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -14,6 +14,43 @@
       </div>
     </div>
     <Banner></Banner>
+    <div class="suscripcion">
+      <div class="uk-container uk-container-center uk-text-center">
+        <h2 class="suscripcion-titulo">Boletines</h2>
+        <p>Anótate para recibir de forma oportuna la información de nuestra programación semanal de actividades culturales (un mensaje a la semana).</p>
+        
+        <form style="padding-top: 20px" @submit="checkForm" method="post" action="https://www.culturapuertomontt.cl/inicio/?na=s" onsubmit="return newsletter_check(this)" target="iframeSuscribcion">
+          <input type="hidden" name="nlang" value="">
+          <input type="hidden" name="nr" value="page">
+          <div class="uk-margin">
+            <input class="uk-input uk-width-1-2" type="text" placeholder="Nombre y apellidos" name="nn" v-model="nameSus">
+          </div>
+          <div class="uk-margin">
+            <input class="uk-input uk-width-1-2" type="email" placeholder="Correo electrónico" name="ne" v-model="emailSus">
+          </div>
+          <div class="tnp-field tnp-field-button"><input class="tnp-submit" type="submit" value="Suscríbete" href="#modal-sus" uk-toggle></div>
+          <!--<div class="tnp-field tnp-field-button"><input class="tnp-submit" type="submit" value="Suscríbete" href="#modal-sus" uk-toggle></div>-->
+          <!--<button class="boton-suscripcion" aria-label="Suscripción" type="submit" value="Suscríbete" href="#modal-sus" uk-toggle>Suscríbete</button>-->
+        </form>
+
+      </div>
+
+      <div id="modal-sus" class="uk-flex-top" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+          <button class="uk-modal-close-default" type="button" uk-close></button>
+
+          <p v-if="errors.length">
+            <b>Por favor, corrija el(los) siguiente(s) error(es):</b>
+            <ul>
+              <li v-for="error in errors">{{ error }}</li>
+            </ul>
+          </p>
+
+          <iframe src="" name="iframeSuscribcion"></iframe>
+        </div>
+      </div>
+    </div>
+
   </section>
 </template>
 
@@ -27,9 +64,35 @@ export default {
     Slider,
     Banner
   },
+  data() {
+    return {
+      errors: [],
+      nameSus: null,
+      emailSus: null
+    }
+  },
   computed: {
     actividades() {
       return this.$store.state.carousel;
+    }
+  },
+  methods: {
+    checkForm: function (e) {
+      this.errors = [];
+      if (this.nameSus && this.emailSus) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.nameSus) {
+        this.errors.push('El nombre es obligatorio.');
+      }
+      if (!this.emailSus) {
+        this.errors.push('La edad es obligatoria.');
+      }
+
+      e.preventDefault();
     }
   }
 }
